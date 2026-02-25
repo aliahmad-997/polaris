60e919c3e43357329060237e2e81d3a1693f2e92
Ali Ahmad <aliahmad_997@outlook.com>
2026-02-26 00:16:15 +0500

    Add FileExplorer component and update project schema

--- src/features/projects/hooks/use-projects.ts
+++ src/features/projects/hooks/use-projects.ts
@@ -44,18 +44,18 @@ export const useCreateProject = () => {
   );
 };
 
-export const useRenameProject = (projectId: Id<"projects">) => {
+export const useRenameProject = () => {
   return useMutation(api.projects.rename).withOptimisticUpdate(
     (localStore, args) => {
       const existingProject = localStore.getQuery(api.projects.getById, {
-        id: projectId,
+        id: args.id,
       });
 
       if (existingProject !== undefined && existingProject !== null) {
         localStore.setQuery(
           api.projects.getById,
           {
-            id: projectId,
+            id: args.id,
           },
           {
             ...existingProject,
@@ -71,7 +71,7 @@ export const useRenameProject = (projectId: Id<"projects">) => {
           api.projects.get,
           {},
           existingProjects.map((project) =>
-            project._id === projectId
+            project._id === args.id
               ? {
                   ...project,
                   name: args.name,
